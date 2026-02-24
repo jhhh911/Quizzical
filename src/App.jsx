fb9feb645cfed8772dc4496d908cff21268592fa
James Heiniger <james.heiniger911@gmail.com>
2026-02-24 16:04:52 +1030

    Importing data via api and added question styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TitlePage from "./title-page";
 
 function App() {
   const [quizStarted, setQuiz] = useState(false);
+  const [allQuestions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+      .then(res => res.json())
+      .then(data => setQuestions(data.results));
+  }, []);
+
+  const listQuestions = allQuestions.map((question, i) => (
+    <div className="question">
+      <p key={i} className="question-text">
+        {question.question}
+      </p>
+      <button>{question.correct_answer}</button>
+      <button>{question.incorrect_answers[0]}</button>
+      <button>{question.incorrect_answers[1]}</button>
+      <button>{question.incorrect_answers[2]}</button>
+    </div>
+  ));
 
   return (
     <main>
       <div className="blob blob-top">
         <img src="../src/img/top.png" />
       </div>
-      {!quizStarted && <TitlePage func={setQuiz}/> }
+      {!quizStarted && <TitlePage func={setQuiz} />}
+
+      {quizStarted && (
+        <section className="questions-list">{listQuestions}</section>
+      )}
       <div className="blob blob-bottom">
         <img src="../src/img/bottom.png" />
       </div>
